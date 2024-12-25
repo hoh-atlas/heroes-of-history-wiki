05feb04b9913bf7e87b2acc2411456d318853a9f
davidvach <vachalek.david@gmail.com>
2024-12-25 23:37:02 +0100

    Analytics Init

--- apps/wiki/src/app/layout.js
+++ apps/wiki/src/app/layout.js
@@ -4,31 +4,37 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "../components/shared/navbar/Navbar";
 import Footer from "../components/shared/footer/Footer";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
 });
 
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
 });
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Heroes of History Wiki</title>
-        <meta name="description" content={"Heroes of History Wiki is a place where you can find information about heroes, battles, events, allied cultures and many written guides to help you better navigate in the game."} />
-      </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+	useEffect(() => {
+		ReactGA.initialize("G-TH20891YX2");
+	}, []);
+	
+	return (
+		<html lang="en">
+		<head>
+			<title>Heroes of History Wiki</title>
+			<meta name="description" content={"Heroes of History Wiki is a place where you can find information about heroes, battles, events, allied cultures and many written guides to help you better navigate in the game."} />
+		</head>
+		<body className={`${geistSans.variable} ${geistMono.variable}`}>
+			<Navbar />
+			{children}
+			<Footer />
+		</body>
+		</html>
+	);
 }
